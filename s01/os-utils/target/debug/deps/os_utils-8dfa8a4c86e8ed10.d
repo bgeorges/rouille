55110e864347aba545f6d3b0d0c87b4b15355304
bgeorges <bruno.georges@gmail.com>
2021-04-28 20:59:41 +0200

    adding dependencies..

---
+++ s01/os-utils/target/debug/deps/os_utils-8dfa8a4c86e8ed10.d
@@ -0,0 +1,5 @@
+/Users/bgeorges/work/rouille/s01/os-utils/target/debug/deps/os_utils-8dfa8a4c86e8ed10: src/main.rs
+
+/Users/bgeorges/work/rouille/s01/os-utils/target/debug/deps/os_utils-8dfa8a4c86e8ed10.d: src/main.rs
+
+src/main.rs:
