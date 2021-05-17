c68b39f17a2ee5f4334f6ce819f9e39e4167c8c8
Bruno Georges <bruno.georges@mac.com>
2021-05-17 23:29:24 +0200

    added safecast-api generated by openapi-generator

---
+++ safecast-api/src/models/mod.rs
@@ -0,0 +1,6 @@
+pub mod measurement;
+pub use self::measurement::Measurement;
+pub mod measurement_input;
+pub use self::measurement_input::MeasurementInput;
+pub mod user;
+pub use self::user::User;
